4c31a8b22669d6ef7ab783fa378e6eec72e63c34
jf nassogomo <nassogomo.jf@gmail.com>
2020-05-06 09:45:39 +0200

    création du projet

---
+++ src/test/java/com/intiformation/jenkins/demo/CalculatriceTest.java
@@ -0,0 +1,26 @@
+package com.intiformation.jenkins.demo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * 
+ * @author JF
+ *
+ */
+public class CalculatriceTest {
+
+	@Test
+	public void testAdd() {
+		Calculatrice cal = new Calculatrice();
+		assertEquals("Est ce que l'addition est correct", 10, cal.add(5, 5));
+	}
+
+	@Test
+	public void testMul() {
+		Calculatrice cal = new Calculatrice();
+		assertEquals("Est ce que la multiplication est correct", 25, cal.mul(5, 5));
+	}
+
+}
